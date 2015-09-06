6ee2e7e3df7f7d7b629415b51ab652a2e8b662f4
Andrea Tupini <tupini07@gmail.com>
2015-09-06 03:00:28 -0600

    dynamic waiting time

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var casper = require('casper').create({
   verbose: false,
   logLevel: "info"
 });
+var username = "username";
+var password = "password";
 
 casper.start('https://godvillegame.com/login/', function() {
   this.echo("Setting up.....", "INFO");
@@ -10,8 +12,8 @@ casper.start('https://godvillegame.com/login/', function() {
 
 casper.waitForSelector("form input[name='username']", function() {
   this.fillSelectors('form[action="/login/login"]', {
-    'input[name = username ]': 'user',
-    'input[name = password ]': 'pass',
+    'input[name = username ]': username,
+    'input[name = password ]': password,
   });
   this.evaluate(function() {
     $('form').submit();
@@ -20,25 +22,32 @@ casper.waitForSelector("form input[name='username']", function() {
 }, true);
 
 casper.then(function() {
-  var who = this;
+  var casp = this;
 
-  var waiting = function(who) {
-    who.wait(200000, function() { //200s wait
-      who.echo("----Getting the amount of GP", "INFO");
+  var waiting = function(casp, sleep_time) {
+    casp.wait(sleep_time, function() { //200s waitW
 
-      gp = parseInt(pwhoop.evaluate(function() {
+      gp = parseInt(casp.evaluate(function() {
         return __utils__.findOne('.gp_val').innerText;
       }).slice(0, -1));
 
-      who.echo("The amount of GP is:  " + gp, "PARAMETER");
+      casp.echo("The amount of GP is:  " + gp, "PARAMETER");
       if (gp >= 25) {
-        who.clickLabel("Encourage");
-        who.echo("Encouraged!!", "PARAMETER");
+        casp.clickLabel("Encourage");
+        casp.echo("Encouraged!!", "COMMENT");
+	gp -= 25;
       }
-      waiting(who);
+    
+      //if there is still GP left
+      if(gp >= 25)
+	waiting(casp, 5000); //just wait 5 seconds
+      else
+	waiting(casp, 500000); //wait 500 seconds	
+    
     });
-  }
-  waiting(who);
+  };
+
+  waiting(casp, 1000);
 });
 
 
